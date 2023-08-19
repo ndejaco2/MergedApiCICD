e37a2c1956a9f95a3148bcef3ba364b29866a739
Nicholas DeJaco <dejacon@amazon.com>
2023-08-19 01:12:59 +0000

    Fixup name for merged api stage

--- lib/mergedApi/book-reviews-mergedapi-stage.ts
+++ lib/mergedApi/book-reviews-mergedapi-stage.ts
@@ -5,6 +5,6 @@ import {BookReviewsMergedApiStack} from "./book-reviews-mergedapi-stack";
 export class BookReviewsMergedApiStage extends cdk.Stage {
     constructor(scope: Construct, id: string, props: cdk.StageProps) {
         super(scope, id, props);
-        const bookReviewsMergedApiStage = new BookReviewsMergedApiStack(this, 'BookServiceStack', props);
+        const bookReviewsMergedApiStage = new BookReviewsMergedApiStack(this, 'BookReviewsMergedApiStack', props);
     }
 }
