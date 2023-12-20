19ca264af2b0d3ba7c67ecec264e081f983cd793
ndejaco2 <ndejaco123@gmail.com>
2023-12-19 16:59:06 -0800

    Remove unused imports for build

--- bin/bookReviewsMergedApi.ts
+++ bin/bookReviewsMergedApi.ts
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import {AuthorsServiceStack} from "../lib/sourceApis/authorsService/authors-service-stack";
-import {ReviewsServiceStack} from "../lib/sourceApis/reviewsService/reviews-service-stack";
 import {BooksServicePipelineStack} from "../lib/sourceApis/booksService/books-service-pipeline-stack";
 import {BookReviewsMergedApiPipeline} from "../lib/mergedApi/book-reviews-mergedapi-pipeline";
 import {AuthorsServicePipelineStack} from "../lib/sourceApis/authorsService/authors-service-pipeline";
--- lib/sourceApis/reviewsService/reviews-service-stage.ts
+++ lib/sourceApis/reviewsService/reviews-service-stage.ts
@@ -1,6 +1,5 @@
 import * as cdk from "aws-cdk-lib";
 import {Construct} from "constructs";
-import {ReviewsServiceStack} from "./reviews-service-stack";
 import { ReviewsServiceApiStack } from "./reviews-service-api-stack";
 
 export class ReviewsServiceStage extends cdk.Stage {
